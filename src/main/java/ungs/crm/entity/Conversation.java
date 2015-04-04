7930b6fa37490e08f55a2f27d1a0f87a5e9cb068
Demian <demian0691@gmail.com>
2015-04-04 02:08:36 -0300

    no hice nada.. parripollo?

---
+++ src/main/java/ungs/crm/entity/Conversation.java
@@ -0,0 +1,49 @@
+package ungs.crm.entity;
+
+public class Conversation {
+	private String tipoContacto,
+				   descripcion,
+				   fecha_Hora;
+	private int idCliente, idContacto;
+
+	public int getIdContacto() {
+		return idContacto;
+	}
+
+	public void setIdContacto(int idContacto) {
+		this.idContacto = idContacto;
+	}
+
+	public int getIdCliente() {
+		return idCliente;
+	}
+
+	public void setIdCliente(int idCliente) {
+		this.idCliente = idCliente;
+	}
+
+	public String getTipoContacto() {
+		return tipoContacto;
+	}
+
+	public void setTipoContacto(String tipoContacto) {
+		this.tipoContacto = tipoContacto;
+	}
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+
+	public String getFecha_Hora() {
+		return fecha_Hora;
+	}
+
+	public void setFecha_Hora(String fecha_Hora) {
+		this.fecha_Hora = fecha_Hora;
+	}	
+	
+}
